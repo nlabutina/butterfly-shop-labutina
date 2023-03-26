326bc133adf4d5b99d3e8b352ac1699a7b152567
nlabutina <n.v.labutina@yandex.ru>
2023-03-26 19:29:32 +0300

    FAQ: разметка, стилизация, аккордеоны, адаптив

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -4,8 +4,20 @@ $(document).ready(function () {
     $('.j-main-menu').toggleClass('is-open');
   })
 
+// Аккордеоны
 
+let prevAccordionBtn = undefined;
 
+$('.j-accordion-btn').on('click', function() {
+
+  if (prevAccordionBtn === this) {
+    $(this).next().slideToggle();
+  } else {
+    $(prevAccordionBtn).next().slideUp();
+    $(this).next().slideDown();
+    prevAccordionBtn = this;
+  }
+})
 
 
 
