214229ac957aa95bd0cd194f7e264fa0f4eeefb2
nlabutina <n.v.labutina@yandex.ru>
2023-04-21 02:11:00 +0300

    Каталог: разметка, стилизация, адаптив, фильтрация

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -4,23 +4,42 @@ $(document).ready(function () {
     $('.j-main-menu').toggleClass('is-open');
   })
 
-// Аккордеоны
+  // Аккордеоны
 
-let prevAccordionBtn = undefined;
+  let prevAccordionBtn = undefined;
 
-$('.j-accordion-btn').on('click', function() {
+  $('.j-accordion-btn').on('click', function () {
 
-  if (prevAccordionBtn === this) {
-    $(this).next().slideToggle();
-  } else {
-    $(prevAccordionBtn).next().slideUp();
-    $(this).next().slideDown();
-    prevAccordionBtn = this;
-  }
+    if (prevAccordionBtn === this) {
+      $(this).next().slideToggle();
+    } else {
+      $(prevAccordionBtn).next().slideUp();
+      $(this).next().slideDown();
+      prevAccordionBtn = this;
+    }
+  })
+
+  // Фильтр в каталоге
+  $('.j-catalogue-link').on('click', function () {
+    $('.j-catalogue-link').removeClass('active');
+    $(this).addClass('active');
+
+    let dataFilter = $(this).data('filter');
+
+    if (dataFilter === 'all') {
+      $('.j-catalogue-item').show();
+      return;
+    }
+
+    $('.j-catalogue-item').each(function () {
+      let dataType = $(this).data('type');
+
+      if (dataFilter === dataType) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    })
+  })
+
 })
-
-
-
-
-})
-
